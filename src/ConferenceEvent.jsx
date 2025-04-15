45f36acd075b598247d4d10469d55f4afb8c76e6
SudoMaster7 <leosca08@gmail.com>
2025-04-15 13:35:05 -0400

    codigo organizado

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -174,48 +174,6 @@ const ConferenceEvent = () => {
           ))}
         </div>
         <div className="total_cost">Total Cost: ${venueTotalCost}</div>
-    
-
-      <div className="addons_selection">
-        {avItems.map((item, index) => (
-            <div className="av_data venue_main" key={index}>
-                <div className="img">
-                    <img src={item.img} alt={item.name} />
-                </div>
-            <div className="text"> {item.name} </div>
-            <div> ${item.cost} </div>
-                <div className="addons_btn">
-                    <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
-                    <span className="quantity-value">{item.quantity}</span>
-                    <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
-                </div>
-            </div>
-        ))}          
-      </div>
-      <div className="total_cost">Total Cost: {avTotalCost}</div>
-
-      <div className="input-container venue_selection">
-        <label htmlFor="numberOfPeople"><h3>Number of People:</h3></label>
-        <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople}
-            onChange={(e) => setNumberOfPeople(parseInt(e.target.value))}
-            min="1"
-        />
-      </div>
-        <div className="meal_selection">
-            {mealsItems.map((item, index) => (
-                <div className="meal_item" key={index} style={{ padding: 15 }}>
-                    <div className="inner">
-                        <input type="checkbox" id={ `meal_${index}` }
-                            checked={ item.selected }
-                            onChange={() => handleMealSelection(index)}
-                        />
-                        <label htmlFor={`meal_${index}`}> {item.name} </label>
-                    </div>
-                    <div className="meal_cost">${item.cost}</div>
-                </div>
-            ))}
-        </div>
-        <div className="total_cost">Total Cost: {mealsTotalCost}</div>
     </div>
                             {/*Necessary Add-ons*/}
                             <div id="addons" className="venue_container container_main">
@@ -227,9 +185,22 @@ const ConferenceEvent = () => {
 
                                 </div>
                                 <div className="addons_selection">
-
+                                    {avItems.map((item, index) => (
+                                        <div className="av_data venue_main" key={index}>
+                                            <div className="img">
+                                                <img src={item.img} alt={item.name} />
+                                            </div>
+                                        <div className="text"> {item.name} </div>
+                                        <div> ${item.cost} </div>
+                                            <div className="addons_btn">
+                                                <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                                                <span className="quantity-value">{item.quantity}</span>
+                                                <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+                                            </div>
+                                        </div>
+                                    ))}          
                                 </div>
-                                <div className="total_cost">Total Cost:</div>
+                                <div className="total_cost">Total Cost: {avTotalCost}</div>
 
                             </div>
 
@@ -243,12 +214,27 @@ const ConferenceEvent = () => {
                                 </div>
 
                                 <div className="input-container venue_selection">
-
+                                    <label htmlFor="numberOfPeople"><h3>Number of People:</h3></label>
+                                        <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople}
+                                            onChange={(e) => setNumberOfPeople(parseInt(e.target.value))}
+                                            min="1"
+                                        />
                                 </div>
                                 <div className="meal_selection">
-
+                                    {mealsItems.map((item, index) => (
+                                        <div className="meal_item" key={index} style={{ padding: 15 }}>
+                                            <div className="inner">
+                                                <input type="checkbox" id={ `meal_${index}` }
+                                                    checked={ item.selected }
+                                                    onChange={() => handleMealSelection(index)}
+                                                />
+                                                <label htmlFor={`meal_${index}`}> {item.name} </label>
+                                            </div>
+                                            <div className="meal_cost">${item.cost}</div>
+                                        </div>
+                                    ))}
                                 </div>
-                                <div className="total_cost">Total Cost: </div>
+                                <div className="total_cost">Total Cost: {mealsTotalCost}</div>
 
 
                             </div>
